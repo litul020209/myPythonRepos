48ba145e64134496e7a9d70435be14b8c7e2199a
Biswaprakashgit.h <litulubiswal@gmail.com>
2025-08-19 01:06:40 +0530

    "commit on 19/08/2025"

--- DSA/SET_DICT/frequencies_usingdict.py
+++ DSA/SET_DICT/frequencies_usingdict.py
@@ -1,10 +1,9 @@
+from collections import defaultdict
 list_01=["a","b","a","c","d","u","f","d","a","b","g","j","h","f","u","k","p","g","l","p"]
-freq={}
+freq = defaultdict(int)
 
 for x in list_01:
-    if x not in freq:
-        freq[x]=1
-    else:
-        freq[x]=freq[x]+1
+        freq[x]+=1
+    
 
 print(freq)
