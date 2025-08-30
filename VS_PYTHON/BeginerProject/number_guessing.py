e03b260374aff323826bcf215729d9dc6184c1e8
Biswaprakashgit.h <litulubiswal@gmail.com>
2025-08-30 06:22:18 +0530

    " just small change"

--- VS_PYTHON/BeginerProject/number_guessing.py
+++ VS_PYTHON/BeginerProject/number_guessing.py
@@ -1,19 +1,6 @@
 import random
 x=random.randint(1,100)
-# while True:
-#     strnum=input("enter your guessing number: ")
-#     if strnum.isnumeric():
-#         num=int(strnum)
-#         if x==num:
-#             print("Congratulation you guess the number. ")
-#             break
-#         elif num>x:
-#             print("Too high!")
-#         elif x>num:
-#             print("Too low! ")
 
-#     else:
-#         print("enter the valid number! ")
 
 while True:
     try:
