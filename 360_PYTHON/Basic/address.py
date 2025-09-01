371d7572b0f181059ea5abed2c0b83f04d2d66f2
Biswaprakashgit.h <litulubiswal@gmail.com>
2025-09-01 16:58:48 +0530

    "360 python some basic question excuation'

---
+++ 360_PYTHON/Basic/address.py
@@ -0,0 +1,8 @@
+a=10
+b=a
+print(id(b))
+print(id(a))
+
+c=12,13,14
+c=type(a)
+print(c)
---
+++ 360_PYTHON/Basic/isInstance.py
@@ -0,0 +1,5 @@
+a=10
+b=10+20j
+print(isinstance(a,type(a)))
+print(isinstance(10+20j,complex))
+print(isinstance("1001",bin))
