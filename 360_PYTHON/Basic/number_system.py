dea6e6f9b5be52b8215a9c55d894d82db7ad2eff
Biswaprakashgit.h <litulubiswal@gmail.com>
2025-09-15 12:19:18 +0530

    "solve a question"

---
+++ 360_PYTHON/Basic/number_system.py
@@ -0,0 +1,16 @@
+a=12
+# print(bin(a))
+# print(oct(a))
+# print(hex(a))
+
+b=bin(a)
+print(b,type(b))
+
+c=0b1100
+print(c,type(c))
+
+a1=input("enter : ")
+print(int(a1,2))
+print(int(a1,8))
+print(int(a1,10))
+print(int(a1,16))
