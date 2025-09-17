4f62b891bc61a2880c91b399133bca8ee6158169
Biswaprakashgit.h <litulubiswal@gmail.com>
2025-09-17 19:05:04 +0530

    "solve the ram sir question"

--- 360_PYTHON/Loop/no_duplicate_in_num.py
+++ 360_PYTHON/Loop/no_duplicate_in_num.py
@@ -1,7 +1,7 @@
 
-my_list=[212,213,202,124,11,121,333,3133,1234,345635,-123,100,4000,199,991,909,101,10001,1002,-1000,0]
+my_list=[1234445555,-789044,111100002223409,]
 res=[]
-c=10
+
 list_0=[]
 list_negative=[]
 zero_negative_index=1
@@ -9,7 +9,7 @@ i=1
 for num in my_list:
     if num <0:
         temp=-num
-        list_negative=list_negative=[zero_negative_index]
+        list_negative=list_negative+[zero_negative_index]
     else:
         temp=num
     length=0
@@ -18,6 +18,7 @@ for num in my_list:
         digit=num%10
         length+=1
         temp //=10
+
     if num%(10**(length-1))==0:
         list_0=list_0+[zero_negative_index]
 
@@ -36,9 +37,10 @@ for num in my_list:
         temp=temp%(10**(length-1))          
         length=length-1
     
+    
     new_num=0
     for x in list1:
-        new_num=new_num*c+x
+        new_num=(new_num*10)+x
         
     if i in list_0:
         new_num=new_num*10
